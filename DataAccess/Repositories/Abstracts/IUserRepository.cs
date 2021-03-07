e1a736e66732d2d9db3131841d1611429ee82f12
denys.polyntsev <denys.polyntsev@itdesk.eu>
2021-03-07 19:20:36 +0100

    Added methods to Interface

--- DataAccess/Repositories/Abstracts/IUserRepository.cs
+++ DataAccess/Repositories/Abstracts/IUserRepository.cs
@@ -8,6 +8,8 @@ namespace DataAccess.Repositories.Abstracts
 {
     public interface IUserRepository : IRepository<User>
     {
+        Task<bool> AddUserAsync(User user);
+        Task<bool> RemoveUserAsync(User user);
         Task<User> GetUserByIdAsync(int id);
         Task<List<User>> GetAllUsersAsync();
         Task<User> GetUserByEmailAsync(string email);
